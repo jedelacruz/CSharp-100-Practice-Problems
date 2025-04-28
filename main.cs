0ca48ebcc9922c047631ed72b7e379fa4cd221b3
Je Dela Cruz <93860350+jedelacruz@users.noreply.github.com>
2025-04-28 12:59:01 +0800

    Create main.cs

---
+++ main.cs
@@ -0,0 +1,199 @@
+// Online C# Editor for free
+// Write, Edit and Run your C# code using C# Online Compiler
+
+using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        
+        // PROBLEM 1
+        
+        /*
+        
+        Check if a number is positive, negative, or zero.
+        ➔ Hint: Use if-else to compare with 0.
+        
+        
+        int num = 4;
+        
+        if(num > 0){
+            Console.WriteLine("number is positive");
+        }
+        
+        else if(num == 0){
+            Console.WriteLine("number is zero");
+        }
+        
+        else{
+            Console.WriteLine("number is negative");
+        }
+        
+        */
+        
+        // PROBLEM 2 
+        
+        /*
+        
+        Check if a number is even or odd.
+        ➔ Hint: Use modulo % 2 operator.
+
+        
+        
+        int num = 2;
+        
+        if(num % 2 == 0){
+            Console.WriteLine("number is even");
+        }
+        else{
+            Console.WriteLine("number is odd");
+        }
+        
+        */
+        
+        // PROBLEM 3
+        
+        /*
+        
+        Find the maximum of two numbers.
+        ➔ Hint: Compare using if-else.
+        
+        
+        
+        int num1 = 2;
+        int num2 = 3;
+        
+        if(num2 > num1){
+            Console.WriteLine("num2 is greater than num1");
+        }
+        else if(num2 == num1){
+            Console.WriteLine("num2 is equal to num1");
+        }
+        else{
+            Console.WriteLine("num1 is greater than num2");
+        }
+        
+        */
+        
+        
+        // PROBLEM 4
+        
+        /*
+        
+        Find the minimum of two numbers.
+        ➔ Hint: Compare using if-else.
+        
+        
+        
+        int num1 = 2;
+        int num2 = 3;
+        
+        if(num2 < num1){
+            Console.WriteLine("num2 is less than num1");
+        }
+        else if(num2 == num1){
+            Console.WriteLine("num2 is equal to num1");
+        }
+        else{
+            Console.WriteLine("num1 is less than num2");
+        }
+        
+        */
+        
+        // PROBLEM 5
+        
+        /*
+        
+        Print numbers from 1 to 10 using a loop.
+        ➔ Hint: Use a while or for loop.
+        
+        int i = 1;
+        while(i <= 10){
+            Console.WriteLine(i);
+            i++;
+        }
+        
+        */
+        
+        // PROBLEM 6
+        
+        /*
+        
+        Print even numbers from 1 to 20.
+        ➔ Hint: Check if number is divisible by 2.
+        
+        int i = 1;
+        
+        while(i <= 20){
+            if(i % 2 == 0){
+                Console.WriteLine(i);
+            }
+            i++;
+        }
+        */
+        
+        // PROBLEM 7
+        
+        /*
+        
+        Find the sum of numbers from 1 to n.
+        ➔ Hint: Add numbers inside a loop.
+        
+        
+        
+        int total = 0;
+        int i = 1;
+        int n = 10;
+        
+        while(i <= n){
+            total += i;
+            i++;
+        }
+        
+        Console.WriteLine(total);
+
+        */
+        
+        // PROBLEM 8
+        
+        /*
+        
+        Display the multiplication table of a given number.
+        ➔ Hint: Multiply inside a loop.
+
+        
+        
+        int i = 0;
+        int n = 5;
+        int sd = 0
+        
+        while()
+        Console.WriteLine($"{i} x {i}")
+        
+        */
+        
+        // PROBLEM 9
+        
+        /*
+        
+        Input age and check if eligible to vote (18+).
+        ➔ Hint: Use if-else statement.
+        
+        int age = 18;
+        if(age >= 18){
+            Console.WriteLine("Eligible to vote");
+        }
+        */
+        
+        // PROBLEM 10
+        
+        /*
+        
+        Check if a number is divisible by 5.
+        ➔ Hint: Use % 5 operator.
+        
+        */
+
+    }
+}
